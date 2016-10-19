3a125710ecc587b3627b7898bed845f89989ff9c
sunhangqi <sunhangqi@gmail.com>
2016-10-19 23:39:03 +0800

    Initial Commit

---
+++ lianhe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lianhe
+//
+//  Created by Macbook on 2016/10/19.
+//  Copyright © 2016年 Macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
