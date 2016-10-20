adb254c28ece44d63eea6db2752afcaee6ef801c
sunhangqi <sunhangqi@gmail.com>
2016-10-20 20:04:46 +0800

    day1
    iOS learning

--- lianhe/ViewController.m
+++ lianhe/ViewController.m
@@ -9,6 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *thePlace;
+@property (strong, nonatomic) IBOutlet UITextField *theVerb;
+@property (strong, nonatomic) IBOutlet UITextField *theNumber;
+@property (strong, nonatomic) IBOutlet UITextView *theTemplate;
+@property (strong, nonatomic) IBOutlet UITextView *theStory;
+@property (strong, nonatomic) IBOutlet UIButton *theButton;
+- (IBAction)createStory:(id)sender;
 
 @end
 
@@ -26,4 +33,6 @@
 }
 
 
+- (IBAction)createStory:(id)sender {
+}
 @end
